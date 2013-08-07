d52c249daf3a4a667a752736a4137f54b48f3236
var114 <patil.varvarian@gmail.com>
2013-08-07 10:40:58 -0700

    Update student.rb

--- student.rb
+++ student.rb
@@ -1,47 +1,45 @@
 class Student 
 
-  attr_reader :first_name, :last_name, :grade 
-	def initialize(first_name, last_name, grade)									     
+  	attr_reader :first_name, :last_name, :grade 
+  	def initialize(first_name, last_name, grade)									     
 		@first_name = first_name 
 		@last_name = last_name 
 		@grade = grade 
 	end
 
-    def senior? #expressing intent
+	def junior?
+		grade == 10
+	end
+
+    def senior? 
     	grade == 12
     end
 
 	def to_s
 		"#{last_name}, #{first_name}" 
-	end
-
-	def junior?
-		grade == 10
-	end
+		"#{last_name}, #{first_name}" 
+	end	
 end 
+
+def junior(students)
+	students.select { |student| student.junior? }
+end
 
 def seniors(students)
 	students.select { |student| student.senior? }
 end
 
-def junior(students)
-	students.select { |student| student.junior? }
-end
 fred = Student.new("Fred", "James", 12 ) 
 sarah = Student.new("Sarah", "Smith", 12)
 jack = Student.new("Jack", "Gong", 10)
-
-
-
 all_students = [fred,sarah, jack] 
-           
- puts 'Seniors'                      		
+
+puts 'Juniors'
+junior(all_students).each do |student|
+	puts student
+end           
+puts 'Seniors'                      		
 seniors(all_students).each do |student|
 	puts student
 end
 
-puts ''
-puts 'Juniors'
-junior(all_students).each do |student|
-	puts student
-end
